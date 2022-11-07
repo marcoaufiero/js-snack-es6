62cf3bc1bacd9f2be4afd3663b8e7249388bb2a7
marcoaufiero <marcoaufiero96@gmail.com>
2022-11-07 14:52:22 +0100

    Jsnack 1 completed using 'filter' method

--- Jsnack 1/main.js
+++ Jsnack 1/main.js
@@ -2,15 +2,31 @@ const myArray = ['Michele', 'Fabio', 'Roberto', 'Giovanni', 'Simone', 'Chiara'];
 
 // forEach Method
 
-let selectedNames = [];
+// let selectedNames = [];
+
+// const min = 2;
+// const max = 4;
+
+
+// myArray.forEach((element, index) => {
+//     if(index >= min && index <= max){
+//         selectedNames.push(element)
+//     }
+// })
+
+// console.log(selectedNames)
+
+
+
+// filter method
 
 const min = 2;
 const max = 4;
 
 
-myArray.forEach((element, index) => {
-    if(index >= min && index <= max){
-        selectedNames.push(element)
+let selectedNames = myArray.filter((element, index) => {
+    if( index >= min && index <= max){
+        return true
     }
 })
 
