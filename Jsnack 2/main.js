c23273e1d4fd1a591251b7a6865b508e507c5c72
marcoaufiero <marcoaufiero96@gmail.com>
2022-11-07 15:17:51 +0100

    Jsnack first task completed

---
+++ Jsnack 2/main.js
@@ -0,0 +1,15 @@
+let students = [
+    { name: 'Marco', id: 213, grades: 78 },
+    { name: 'Paola', id: 110, grades: 96 },
+    { name: 'Andrea', id: 250, grades: 48 },
+    { name: 'Gaia', id: 145, grades: 74 },
+    { name: 'Luigi', id: 196, grades: 68 },
+    { name: 'Piero', id: 102, grades: 50 },
+    { name: 'Francesca', id: 120, grades: 84 },
+];
+
+let labels = students.map((element) =>{
+    return element.name.toUpperCase()
+})
+
+console.log(labels)
