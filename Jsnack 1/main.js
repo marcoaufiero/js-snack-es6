68aa9959ec03efcf577f132ce85933ca10515081
marcoaufiero <marcoaufiero96@gmail.com>
2022-11-07 14:47:31 +0100

    Jsnack 1 completed using 'forEach' method

--- Jsnack 1/main.js
+++ Jsnack 1/main.js
@@ -1,2 +1,17 @@
 const myArray = ['Michele', 'Fabio', 'Roberto', 'Giovanni', 'Simone', 'Chiara'];
 
+// forEach Method
+
+let selectedNames = [];
+
+const min = 2;
+const max = 4;
+
+
+myArray.forEach((element, index) => {
+    if(index >= min && index <= max){
+        selectedNames.push(element)
+    }
+})
+
+console.log(selectedNames)
